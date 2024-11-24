7b101b6797c4cd2675f5b102a93a49bd86f13719
Apsara2001 <139642058+Apsara2001@users.noreply.github.com>
2024-11-24 01:56:10 +0000

    Update GameTable component ,added useState hooks and animal data import to GameTable and implement handleClick function to GameTable component.

---
+++ ass4/src/components/GameTable.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import '../assets/css/main.css'
+import {animals} from '../assets/data/data.js'
+
+const GameTable = () => {
+    
+    const [animalName,setAnimalName] = useState('');
+    const [gameStatus,setGameStatus] = useState("");
+
+    
+   
+     
+      const handleClick=((e)=>{
+        if(e === animalName.name){
+            setGameStatus('WIN')
+            setTimeout(() => {
+                randomAnimalName()
+            }, 2000);
+          
+        }else{
+            setGameStatus('LOSS')
+        }
+    })
+  return (
+    <>
+    <div className='container'>
+        
+        <table>
+           
+            <thead>
+                <tr >
+                    <th colSpan={3}>ANIMAL MATCHING GAME</th>
+                </tr>
+                <tr >
+                    <th>Result</th>
+                    <th>Animal Name</th>
+                    <th>Selecte the animal</th>
+                </tr>
+            </thead>
+           
+            <tbody>
+                <tr>
+                  
+                <td className='names' style={gameStatus==='WIN'? {color:'#67de49'}:{color:'red'} }>{gameStatus}</td>
+                
+                <td>{animalName.name}</td>
+               
+                    <td><div>
+                         <div className='imgtags'>{animals.map(animal=>
+                        <div  onClick={()=>handleClick(animal.name)}>
+                            <img src={require(`../assets/images/${animal.img}`)} alt='img' className='imgCard' />
+                            </div>
+                        
+                    )}</div></div></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </>
+  )
+}
+
+export default GameTable
